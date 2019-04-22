90a717fb44cd6558a4ab4888f6f6c6fc08a6f394
Kalana <kalana.16@cse.mrt.ac.lk>
2019-04-22 14:02:09 +0530

    cleared uid on closing the browser

--- background_scripts/onRemoved.js
+++ background_scripts/onRemoved.js
@@ -1,3 +1,3 @@
 chrome.windows.onRemoved.addListener(function(windowid) {
-    chrome.storage.local.remove(['loggedIn','credentials','urls']);
+    chrome.storage.local.remove(['loggedIn','credentials','urls','uid']);
    });
